0bf8b6c7b5323a83e426bb577df63c2ee1bc6e2b
Carlo Eugene Manalo <carloeugene@gmail.com>
2020-05-05 21:40:55 +0800

    Home screen input validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Home from './components/Home'
 import Main from './components/Main'
 import ScoreBoard from './components/admin/ScoreBoard'
+import socketIOClient from "socket.io-client";
 
 import './styles/home.css'
 
+const ENDPOINT = "http://127.0.0.1:4001";
+
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <Home />
       {/* <Main /> */}
       {/* <ScoreBoard /> */}
-      
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, TextField, Button } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import AppleIcon from '@material-ui/icons/Apple';
 
 export default function Home () {
+  const [ roomId, setRoomId ] = useState("")
+  const [ name, setName ] = useState("")
+  const [ isFormValid, setIsFormValid ] = useState(false)
+
+  useEffect(() => {
+    setIsFormValid(roomId && name)
+  }, [roomId, name])
+
+  const onRoomIdChange = (event) => {
+    const value = event.target.value
+    setRoomId(value.toUpperCase())
+  }
+
+
+  const onNameChange = (event) => {
+    setName(event.target.value)
+  }
+
   return <Container maxWidth="sm">
     <div className="title">
       <span>G<AppleIcon style={{ color: "red" }}/>rden of Eden</span>
     </div>
     <div className="form">
       <div className="roomId">
-        <TextField id="roomIdText" label="Room Id" />
+        <TextField id="roomIdText" label="Room Id" onChange={onRoomIdChange} value={roomId} required autoFocus />
       </div>
       <div className="name">
-        <TextField id="nameText" label="Name" />
+        <TextField id="nameText" label="Name" onChange={onNameChange} value={name} required />
       </div>
       <div className="submitButton">
-        <Button variant="contained" color="primary" endIcon={<SendIcon />}>
+        <Button variant="contained" color="primary" endIcon={<SendIcon />} disabled={!isFormValid}>
           Submit
         </Button>
       </div>
